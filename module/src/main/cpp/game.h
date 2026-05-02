4aa3373404d4130c4c95c2791a967f68aa58ef50
divarvian <divarvian@gmail.com>
2026-05-02 19:50:30 +0700

    feat: support all mlbb version

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,29 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.mobile.legends:UnityKillsMe"
+#include <cstring>
+
+// Array of supported game package names
+static const char* GamePackageNames[] = {
+    "com.mobile.legends:UnityKillsMe",
+    "com.mobile.legends.usa:UnityKillsMe",
+    "com.mobile.legends.lite:UnityKillsMe",
+    "com.hhgame.mlbbvn:UnityKillsMe",
+    "com.mobiin.gp:UnityKillsMe"
+};
+
+static const int GamePackageNamesCount = sizeof(GamePackageNames) / sizeof(GamePackageNames[0]);
+
+// Function to check if package name is supported
+static bool isGamePackage(const char* package_name) {
+    if (package_name == nullptr) return false;
+    
+    for (int i = 0; i < GamePackageNamesCount; i++) {
+        if (strcmp(package_name, GamePackageNames[i]) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
 #endif // ZYGISK_IL2CPPDUMPER_GAME_H
